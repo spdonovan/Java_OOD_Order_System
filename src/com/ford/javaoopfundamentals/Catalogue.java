6b07333f126e4cdaff1981f1f04a972c760f53db
ScottDonovan <spdonovan@hotmail.co.uk>
2022-01-25 16:24:48 +0000

    Customer orders can be created and listed

--- src/com/ford/javaoopfundamentals/Catalogue.java
+++ src/com/ford/javaoopfundamentals/Catalogue.java
@@ -6,7 +6,8 @@ import java.util.Map;
 public class Catalogue {
     private static Map<String, Product> productMap = Map.of(
             "Electric Toothbrush", new Product("Electric Toothbrush", 6550),
-            "Cat Food", new Product("Tuna CatFood", 350));
+            "Cat Food", new Product("Tuna CatFood", 350),
+            "Animal Farm (e-book)", new Product("Animal Farm - Orwell", 750));
 
     public static Product getProduct(String productName) {
         return productMap.get(productName);
--- src/com/ford/javaoopfundamentals/Demo.java
+++ src/com/ford/javaoopfundamentals/Demo.java
@@ -1,14 +1,22 @@
 package com.ford.javaoopfundamentals;
 
+import java.util.Optional;
+
 public class Demo {
     public static void main(String[] args) {
-        ShoppingCart cart = new ShoppingCart();
                 Product toothbrush = Catalogue.getProduct("Electric Toothbrush");
                 Product catfood = Catalogue.getProduct("Cat Food");
-                cart.addProduct(toothbrush);
-                cart.addProduct(catfood);
-                System.out.println(cart);
+                Product animalFarm = Catalogue.getProduct("Animal Farm (e-book)");
+
+                ShoppingCart cart = new ShoppingCart();
+                cart.addLineItem(new LineItem(catfood, 1));
+                cart.addLineItem(new LineItem(animalFarm, 1));
+                LineItem toothbrushes = new LineItem(toothbrush, 2);
+                cart.addLineItem(toothbrushes);
                 System.out.println(cart.getTotalCost());
 
+                Customer scottDonovan = new Customer("Scott Donovan", 5432234567529854L);
+                Optional<Order> order = scottDonovan.checkout(cart);
+                System.out.println(order);
     }
 }
