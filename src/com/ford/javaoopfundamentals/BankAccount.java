6334e10a17b9d57c13bf3a439f8fb6aa1b1664bd
ScottDonovan <spdonovan@hotmail.co.uk>
2022-01-26 11:46:33 +0000

    Bank account class added and customer refactored for payment methods

---
+++ src/com/ford/javaoopfundamentals/BankAccount.java
@@ -0,0 +1,28 @@
+package com.ford.javaoopfundamentals;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class BankAccount implements PaymentMethod {
+
+    private final int sortCode;
+    private final long accountNumber;
+
+    BankAccount(int sortCode, int accountNumber) {
+        this.sortCode = sortCode;
+        this.accountNumber = accountNumber;
+
+    }
+
+    @Override
+    public Optional<Payment> mkPayment(int value)
+    { return Optional.of(new Payment(this, value, UUID.randomUUID()));}
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "sortCode=" + sortCode +
+                ", accountNumber=" + accountNumber +
+                '}';
+    }
+}
