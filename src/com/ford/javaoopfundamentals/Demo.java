10e11cab99ce7937d9fa811c925ad435de1279b3
ScottDonovan <spdonovan@hotmail.co.uk>
2022-01-26 17:17:20 +0000

    changes to Demo for testing

--- src/com/ford/javaoopfundamentals/Demo.java
+++ src/com/ford/javaoopfundamentals/Demo.java
@@ -1,18 +1,20 @@
 package com.ford.javaoopfundamentals;
 
-import java.util.Optional;
 
 public class Demo {
     public static void main(String[] args) {
                 Product toothbrush = Catalogue.getProduct("Electric Toothbrush");
-                Product catfood = Catalogue.getProduct("Cat Food");
+                Product catFood = Catalogue.getProduct("Cat Food");
                 Product animalFarm = Catalogue.getProduct("Animal Farm (e-book)");
+                Product sofa = Catalogue.getProduct("Sofa");
 
                 ShoppingCart cart = new ShoppingCart();
-                cart.addLineItem(new LineItem(catfood, 1));
+                cart.addLineItem(new LineItem(catFood, 1));
                 cart.addLineItem(new LineItem(animalFarm, 1));
                 LineItem toothbrushes = new LineItem(toothbrush, 2);
                 cart.addLineItem(toothbrushes);
+                LineItem newSofa = new LineItem(sofa, 1);
+                cart.addLineItem(newSofa);
                 System.out.println(cart.getTotalCost());
 
     }
